285b13113cd50e333ff6ed3384c70190e3f8e271
Anthony Albertorio <anthony.albertorio@gmail.com>
2015-11-13 22:15:30 -0500

    finished 18-Diff of Two Arrays bonfire. Didn't use splice or filter.

---
+++ Bonfires/Intermediate-Algorithm-Scripting/18-DiffTwoArrays.js
@@ -0,0 +1,43 @@
+function diff(arr1, arr2) {
+  // compare each element to each other
+    // cycle one element of first array
+    // each element of first array compared to all elements
+    // in second array
+    
+  // if elements match,
+    // eliminate all instances of element
+    // in both arrays
+    
+    var arr1Temp = arr1;
+    var arr2Temp = arr2;
+    var finalArr = [];
+    
+    for(var i = 0; i < arr1.length; i++){
+        for(var j = 0; j < arr2.length; j++){
+            console.log("arr1:"+ arr1[i] + 
+                        " compared to arr2:" + arr2[j]);
+            if(arr1[i] === arr2[j]){
+                console.log("Match!");
+                delete arr2Temp[j];
+                delete arr1Temp[i];
+                break;
+            }
+        }
+        console.log("\n");
+    }
+    
+    var newArr = arr1Temp.concat(arr2Temp);
+    
+    for(var k = 0; k < newArr.length; k++){
+        if(newArr[k] !== undefined){
+            finalArr.push(newArr[k]);
+            console.log("got ya! " + newArr[k]);
+        }
+    }
+    
+    return finalArr;
+
+}
+
+diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
